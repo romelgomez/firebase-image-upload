acdb66b687c79b857841ed0ac23e5a76cb323b0f
Romel Javier Gomez Herrera <bmxquiksilver7185@gmail.com>
2015-07-15 16:07:57 -0430

    some advances

--- app/scripts/publications.js
+++ app/scripts/publications.js
@@ -73,6 +73,9 @@ angular.module('publications',['tree','filters','ngMessages','angular-redactor']
 
     $scope.save = function(validate){
 
+      // snapshots of publications
+      // releases
+
       // [snapshots] luego de presionar <Publish>, y cada vez que el vendedor presione el botón <Update>, se crea una instantánea de la publicación.
       // Cuando el cliente confirma el contrato, lo hará sobre la última instantánea guardada. El comprador tendrá derechos y deberes según
       // lo especifique la instantánea. Si el vendedor luego modifica la publicación, estará creado otra instantánea o contrato.
@@ -83,23 +86,41 @@ angular.module('publications',['tree','filters','ngMessages','angular-redactor']
       // <Update> <Pause or Enable> <Delete>
       // <Publish> <Save> <Discard>
       //
-      // Publish & Update  -> Valida que las fotos y los campos del formulario esten completos
-      // Save              -> Toma lo que este al momento y guarda
+      // Publish & Update  -> validate = true;  Valida que las fotos y los campos del formulario esten completos
+      // Save              -> validate = false; Toma lo que este al momento y guarda
       // Pause             -> actualiza un campo
       // Enable            -> actualiza un campo
       // Delete            -> actualiza un campo
       // Discard           -> actualiza un campo
 
-      // id, user_id, title, body, 	price, quantity, status, published, banned, deleted, created, modified
+      // publications           snapshots
+
+      // category               category
+      // userId                 userId
+      // title                  title
+      // description            description
+      // price                  price
+      // quantity               quantity
+      // barcode                barcode
+      // warranty               warranty
+      //                        termsOfService
+      // paused                                     true or false
+      // released                                   true or false
+      // deleted                                    true or false
+      // created  (draft)       created (released)             Firebase.ServerValue.TIMESTAMP
 
       //var function
 
       if(validate){
         if($scope.form.$valid){
-
+          // update
         }
       }else{
-
+        if(key === ''){
+          // add a new record
+        }else{
+          // update
+        }
       }
 
       //timestamp: Firebase.ServerValue.TIMESTAMP
