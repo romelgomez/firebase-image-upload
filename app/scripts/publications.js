bc13f9e002e0a00c4035929b60bdf737c082eb64
Romel Javier Gomez Herrera <bmxquiksilver7185@gmail.com>
2015-07-17 15:53:42 -0430

    some advances

--- app/scripts/publications.js
+++ app/scripts/publications.js
@@ -14,7 +14,7 @@ angular.module('publications',['tree','filters','ngMessages','angular-redactor']
     };
 
   }])
-  .controller('PublicationsController',['$scope','tree','notificationService','$filter','$log',function($scope,tree,notificationService,$filter,$log){
+  .controller('PublicationsController',['$scope','tree','publicationsService','notificationService','$filter','$log',function($scope,tree,publicationsService,notificationService,$filter,$log){
 
     $scope.treeNodes = tree.nodes();
 
@@ -34,11 +34,17 @@ angular.module('publications',['tree','filters','ngMessages','angular-redactor']
     // id, user_id, title, body, 	price, quantity, status, published, banned, deleted, created, modified
 
     var original = angular.copy($scope.model = {
-      category:     '',
+      userId:       null,
+      categoryId:   null,
       title:        null,
       description:  null,
       price:        null,
-      quantity:     null
+      quantity:     null,
+      barcode:      null,
+      warranty:     null,
+      releaseDate:  null,
+      paused:       null,
+      deleted:      null
     });
 
     $scope.reset = function(){
@@ -47,108 +53,58 @@ angular.module('publications',['tree','filters','ngMessages','angular-redactor']
       $scope.form.$setPristine();
     };
 
+    var id = '';
+
+    var publications = publicationsService.publications();
+
+    $scope.httpRequestPromise = publications.$loaded(null,function(error){
+      notificationService.error(error);
+    });
+
     /**
      @Name
      @Description
      @parameters
      @returns
      */
-    $scope.submit = function () {
-      if($scope.form.$valid){
-
-        $log.log('$scope.model',$scope.model);
-
-        //$scope.httpRequestPromise = $scope.nodes.$add(node).then(function() {
-        //  notificationService.success('Data has been saved to our Firebase database');
-        //  $scope.reset();
-        //},function(error){
-        //  notificationService.error(error);
-        //  //$window.location = '/';
-        //});
-
-      }
-    };
-
-    var key = '';
-
     $scope.save = function(validate){
 
-      /*
+      //if(validate){
+      //  if($scope.form.$valid){
+      //    // update
+      //    $scope.model.releaseDate = Firebase.ServerValue.TIMESTAMP;
+      //
+      //
+      //  }
+      //}else{
+      //  if(id === ''){
+      //    $scope.httpRequestPromise = publications.$add($scope.model).then(function(ref) {
+      //      key = ref.key();
+      //      notificationService.success('Data has been saved to our Firebase database');
+      //    },function(error){
+      //      notificationService.error(error);
+      //    });
+      //    // add a new record
+      //  }else{
+      //    // update
+      //
+      //    var record = publications.$getRecord(key);
+      //
+      //
+      //
+      //    publications.$save(record).then(function() {
+      //      notificationService.success('Data has been update to our Firebase database');
+      //    },function(error){
+      //      notificationService.error(error);
+      //    });
+      //
+      //  }
+      //}
 
-       snapshots of publications
-       releases
-
-       cuando el usuario confirma la compra se crea un instantánea de todos los datos.
-
-
-       [snapshots] luego de presionar <Publish>, y cada vez que el vendedor presione el botón <Update>, se crea una instantánea de la publicación.
-       Cuando el cliente confirma el contrato, lo hará sobre la última instantánea guardada. El comprador tendrá derechos y deberes según
-       lo especifique la instantánea. Si el vendedor luego modifica la publicación, estará creado otra instantánea o contrato.
-
-       Las modificaciones realizadas a la publicación se reflejarán en todos los clientes que la estén visualizando, tan rápido como la
-       latencia de la conexión de internet lo permita.
-
-       <Update> <Pause or Enable> <Delete>
-       <Publish> <Save> <Discard>
-
-       Publish & Update  -> validate = true;  Valida que las fotos y los campos del formulario esten completos
-       Save              -> validate = false; Toma lo que este al momento y guarda
-       Pause             -> actualiza un campo
-       Enable            -> actualiza un campo
-       Delete            -> actualiza un campo
-       Discard           -> actualiza un campo
-
-       publications           snapshots
-
-       category               category
-       user
-       title                  title
-       description            description
-       price                  price
-       quantity               quantity
-       barcode                barcode
-       warranty               warranty
-                              termsOfService
-       paused                                     true or false
-       releaseDate                                Firebase.ServerValue.TIMESTAMP
-       deleted                                    true or false
-       created  (draft) created (released)        Firebase.ServerValue.TIMESTAMP
-
-       Main Categories [Market, Jobs, Real Estate, Vehicles] // Boats, Planes, stock market
-
-       <publications>
-
-       category               string
-       user                   string
-       title                  string
-       description            string
-       price                  int
-       quantity               int
-       barcode                string
-       warranty               string
-       releaseDate            date
-       paused                 boolean
-       deleted                boolean
-
-
-
-
-      */
-
-      if(validate){
-        if($scope.form.$valid){
-          // update
-        }
-      }else{
-        if(key === ''){
-          // add a new record
-        }else{
-          // update
-        }
-      }
-
-      //timestamp: Firebase.ServerValue.TIMESTAMP
     };
+
+  }]);
+
 
 
 //    var messages = $FirebaseArray(ref);
@@ -169,16 +125,88 @@ angular.module('publications',['tree','filters','ngMessages','angular-redactor']
 //      // data has been saved to our database
 //    });
 
-    //var publications = [
-    //  {
-    //    "left": 1,
-    //    "name": "321",
-    //    "parentId": "",
-    //    "right": 4,
-    //    "$id": "-Jtj9bAlDopYZTwv_PNU",
-    //    "$priority": null
-    //  },
+//var publications = [
+//  {
+//    "left": 1,
+//    "name": "321",
+//    "parentId": "",
+//    "right": 4,
+//    "$id": "-Jtj9bAlDopYZTwv_PNU",
+//    "$priority": null
+//  },
+
+
+/*
+
+ snapshots of publications
+ releases
+
+ cuando el usuario confirma la compra se crea un instantánea de todos los datos.
+
+
+ [snapshots] luego de presionar <Publish>, y cada vez que el vendedor presione el botón <Update>, se crea una instantánea de la publicación.
+ Cuando el cliente confirma el contrato, lo hará sobre la última instantánea guardada. El comprador tendrá derechos y deberes según
+ lo especifique la instantánea. Si el vendedor luego modifica la publicación, estará creado otra instantánea o contrato.
+
+ Las modificaciones realizadas a la publicación se reflejarán en todos los clientes que la estén visualizando, tan rápido como la
+ latencia de la conexión de internet lo permita.
+
+ <Update> <Pause or Enable> <Delete>
+ <Publish> <Discard>
+
+ Publish & Update  -> validate = true;  Valida que las fotos y los campos del formulario esten completos
+ Save              -> validate = false; Toma lo que este al momento y guarda
+ Pause             -> actualiza un campo
+ Enable            -> actualiza un campo
+ Delete            -> actualiza un campo
+ Discard           -> actualiza un campo
+
+ publications           snapshots
+
+ category               category
+ user
+ title                  title
+ description            description
+ price                  price
+ quantity               quantity
+ barcode                barcode
+ warranty               warranty
+
+ paused                                     true or false
+ releaseDate                                Firebase.ServerValue.TIMESTAMP
+ deleted                                    true or false
+ created  (draft) created (released)        Firebase.ServerValue.TIMESTAMP
+
+ termsOfService
+
+ Main Categories [Market, Jobs] // Real Estate, Vehicles, Boats, Planes, stock market
+
+ <publications> Market
+
+ userId                 string
+ categoryId             string
+ title                  string
+ description            string
+ price                  int
+ quantity               int
+ barcode                string
+ warranty               string
+ releaseDate            date
+ paused                 boolean
+ deleted                boolean
+
+ <publications> Jobs
+
+ userId                 string
+ categoryId             string
+ title                  string
+ description            string
+ salary
+ barcode                string
+ releaseDate            date
+ paused                 boolean
+ deleted                boolean
 
 
 
-  }]);
+ */
