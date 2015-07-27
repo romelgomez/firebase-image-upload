ef75724376356029f53c72f9274b9b296b24da04
romelgomez <bmxquiksilver7185@gmail.com>
2015-07-27 08:20:06 -0430

    some advances

--- app/scripts/ngUpload.js
+++ app/scripts/ngUpload.js
@@ -1,3 +1,4 @@
+// https://w3c.github.io/FileAPI/#FileReader-interface
 // https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications
 // https://developer.mozilla.org/en-US/docs/Web/API/FileReader
 // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
