c700fb266b2f1e88dae1e489fdf339ee8f827cce
Romel Javier Gomez Herrera <bmxquiksilver7185@gmail.com>
2015-07-27 11:21:28 -0430

    some advances

--- app/scripts/ngUpload.js
+++ app/scripts/ngUpload.js
@@ -4,32 +4,22 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
 
 angular.module('ngUpload',[])
-  .controller('NgUploadController',['$scope',function($scope){
-
-    $scope.queueFiles = [];
-
-    //scope.$watch(function(scope) { return scope.queueFiles; },
-    //  function() {
-    //
-    //    //angular.forEach(scope.queueFiles, function(value,index){
-    //    //    //value
-    //    //});
-    //  }
-    //);
-
-
-  }])
   .directive('ngUpload', ['$q','$window','rfc4122','$log',function ($q,$window,rfc4122,$log) {
 
     var controller = function ($scope){
+
+      $scope.queueFiles = {};
+
+      $scope.removeAllQueueFiles = function(){
+        $scope.queueFiles = {};
+      };
+
     };
 
     return {
       restrict: 'E',
       templateUrl: 'ngUpload.html',
-      scope: {
-        queueFiles:'='
-      },
+      scope: {},
       controller:controller,
       link: function(scope, element) {
 
@@ -38,71 +28,29 @@ angular.module('ngUpload',[])
           angular.forEach(event.target.files,function(file){
             var uuid = rfc4122.v4();
             scope.queueFiles[uuid] =  {
-              file:file
+              file: file,
+              preview:'images/loading.gif'
             };
-            var reader = new FileReader();
-            reader.onloadstart = function () {
-              scope.$apply(function () {
-                scope.queueFiles[[uuid]].preview = 'images/loading.gif';
-              });
-            };
-            reader.onload = function (loadEvent) {
-              scope.$apply(function () {
-                scope.queueFiles[[uuid]].preview = loadEvent.target.result;
-              });
-            };
-            reader.readAsDataURL(file);
+
+            scope.$apply(function(scope){
+                var reader = new FileReader();
+                reader.onerror = function(){
+                  // The reading operation encounter an error.
+                  scope.queueFiles[uuid].preview        = 'images/error.png';
+                  scope.queueFiles[uuid].previewStatus  = 'error';
+                  scope.$apply();
+                };
+                reader.onload = function (loadEvent) {
+                  // The reading operation is successfully completed.
+                  scope.queueFiles[uuid].preview        = loadEvent.target.result;
+                  scope.queueFiles[uuid].previewStatus  = 'success';
+                  scope.$apply();
+                };
+                reader.readAsDataURL(file);
+            });
+
           });
         });
-
-
-        //scope.queueFiles = scope.queueFiles ? scope.queueFiles : [];
-        //element.on('change', function (event) {
-        //  angular.forEach(event.target.files,function(file){
-        //    var reader = new FileReader();
-        //    reader.onload = function (loadEvent) {
-        //      scope.$apply(function () {
-        //        scope.queueFiles.push({
-        //          preview:loadEvent.target.result,
-        //          file:file
-        //        });
-        //      });
-        //    };
-        //    reader.readAsDataURL(file);
-        //  });
-        //});
-
-
-
-        //scope.queueFiles = scope.queueFiles ? scope.queueFiles : [];
-        //element.on('change', function (event) {
-        //  angular.forEach(event.target.files,function(file){
-        //    scope.queueFiles.push({
-        //      preview:'images/loading.gif',
-        //      file:file
-        //    });
-        //  });
-        //  scope.$apply();
-        //  //$log.log('queueFiles',scope.queueFiles);
-        //});
-
-
-
-        //scope.queueFiles = scope.queueFiles ? scope.queueFiles : [];
-        //element.on('change', function (event) {
-        //  angular.forEach(event.target.files,function(file){
-        //    scope.queueFiles.push({
-        //      preview:'images/loading.gif',
-        //      file:file
-        //    });
-        //  });
-        //  scope.$apply();
-        //  $log.log('queueFiles',scope.queueFiles);
-        //});
-
-
-
-        //$log.log('changeEvent.target.files',event.target.files);
 
       }
     };
