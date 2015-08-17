3c7abbadf509f9712e86783b45e32c8cf1f483fe
romelgomez <bmxquiksilver7185@gmail.com>
2015-08-17 10:38:45 -0430

    some advances

--- app/scripts/fileUpload.js
+++ app/scripts/fileUpload.js
@@ -19,17 +19,13 @@
 // TODO - Remove ALL files, in queue to upload and those already in server.
 // TODO - Remove THE file, in queue to upload or that it is already in the server.
 
+/******* FireBase Data Base Structure  *******
 
-angular.module('fileUpload',[])
-  .factory('fireBaseService',[function(){
+ Publications Path:
+ publications/fireBaseUniqueIdentifier/images/uuid/deleted
+ publications/fireBaseUniqueIdentifier/images/uuid/name
 
-    /******* FireBase Data Base Structure  *******
-
-     Publications Path:
-     publications/fireBaseUniqueIdentifier/images/uuid/deleted
-     publications/fireBaseUniqueIdentifier/images/uuid/name
-
-     publications:{
+ publications:{
               fireBaseUniqueIdentifier:{
                 title:'publication title',
                 description:'publication description',
@@ -50,11 +46,11 @@ angular.module('fileUpload',[])
               }
             }
 
-     Images Paths:
-     images/uuid/thumbnails/w200xh200
-     images/uuid/thumbnails/w600xh600
+ Images Paths:
+ images/uuid/thumbnails/w200xh200
+ images/uuid/thumbnails/w600xh600
 
-     images:{
+ images:{
               uuid:{
                 thumbnails:{
                   w200xh200:'base64 string',
@@ -63,110 +59,43 @@ angular.module('fileUpload',[])
               }
             }
 
-     **/
+ **/
 
-    return {};
+angular.module('fileUpload',[])
+  .controller('FileUploadController', ['$scope','$q','rfc4122','fileService','$log',function ($scope,$q,rfc4122,fileService,$log) {
 
-  }])
-  .controller('FileUploadController', ['$scope','$q','rfc4122','FireRef','$firebaseObject','fileUploadService','$log',function ($scope,$q,rfc4122,FireRef,$firebaseObject,fileUploadService,$log) {
-
-    var fixedFireBaseUniqueIdentifier = '-Juqip8bcmF7u3z97fbe';
-    var publicationImagesReference  = FireRef.child('publications').child(fixedFireBaseUniqueIdentifier).child('images');
-    var publicationImagesObject = $firebaseObject(publicationImagesReference);
-
-    fileUploadService.files().then(function(_files_) {
+    fileService.files().then(function(_files_) {
       $scope.files  = _files_;
     });
 
     $scope.filesLength  = function(){
-      return fileUploadService.filesLength();
+      return fileService.filesLength();
     };
 
     $scope.queueFiles = function(){
-      return fileUploadService.queueFiles();
+      return fileService.queueFiles();
     };
 
     $scope.progressInstances = {};
 
     $scope.uploadFiles = function(){
-      fileUploadService.files().then(function(files) {
+      fileService.files().then(function(files) {
         angular.forEach(files,function(fileObject,reference){
           if(fileObject.inServer === false){
-            var promises = {
-              reference:          $q.when(reference),
-              fileName:           $q.when(fileObject.fileName),
-              w200xh200Thumbnail: fileUploadService.generateThumbnail(fileObject.preview,200,200,0.7),
-              w600xh600Thumbnail: fileUploadService.generateThumbnail(fileObject.preview,600,600,1.0)
-            };
-
-            $q.all(promises).then(function(the){
-              var reference           = the.reference;
-              var fileName            = the.fileName;
-              var w200xh200Thumbnail  = the.w200xh200Thumbnail;
-              var w600xh600Thumbnail  = the.w600xh600Thumbnail;
-
-              var imagesRef  = FireRef.child('images').child(reference);
-              var imageObj   = $firebaseObject(imagesRef);
-
-              imageObj.name = fileName;
-              imageObj.thumbnails = {};
-              imageObj.thumbnails.w200xh200 = {};
-              imageObj.thumbnails.w200xh200.reference = reference;
-              imageObj.thumbnails.w200xh200.base64     = w200xh200Thumbnail;
-
-              imageObj.thumbnails.w600xh600 = {};
-              imageObj.thumbnails.w600xh600.reference = reference;
-              imageObj.thumbnails.w600xh600.base64     = w600xh600Thumbnail;
-
-              publicationImagesObject[reference]           = {};
-              publicationImagesObject[reference].name      = fileName;
-              publicationImagesObject[reference].isDeleted = false;
-
-              var savingOperations = {
-                inImages: imageObj.$save().then(function(ref) {
-                  var reference = ref.key();
-                  fileUploadService.updateFileObj(reference,{inServer:true});
-                }),
-                inPublications: publicationImagesObject.$save()
-              };
-
-              $scope.progressInstances[reference] = $q.all(savingOperations);
-            });
-          }
-        });
-      });
-
-    };
-
-    var filesInServer = function(){
-      publicationImagesObject.$watch(function() {
-        angular.forEach(publicationImagesObject,function(fileObj,reference){
-          if(!fileObj.isDeleted){
-            fileUploadService.files().then(function(files) {
-              if(!angular.isDefined(files[reference])){
-                var file = {
-                  fileName: fileObj.name,
-                  preview:  'images/loading.jpeg',
-                  inServer: true
-                };
-                fileUploadService.insertFile(reference,file);
-
-                var w200xh200ThumbnailReference = FireRef.child('images').child(reference).child('thumbnails').child('w200xh200');
-                var imageObj  = $firebaseObject(w200xh200ThumbnailReference);
-                imageObj.$loaded(function(thumbnailObject){
-                  fileUploadService.updateFileObj(thumbnailObject.reference,{preview:thumbnailObject.base64})
-                });
-              }
-            });
+            $scope.progressInstances[reference] = $q.when(fileService.upload(fileObject,reference));
           }
         });
       });
     };
 
-    filesInServer();
+    fileService.filesInServer();
 
   }])
-  .factory('fileUploadService',['$q','rfc4122','$log',function($q,rfc4122,$log){
+  .factory('fileService',['$q','rfc4122','FireRef','$firebaseObject','$log',function($q,rfc4122,FireRef,$firebaseObject,$log){
+
+    var fixedFireBaseUniqueIdentifier = '-Juqip8bcmF7u3z97fbe';
+    var publicationImagesReference = FireRef.child('publications').child(fixedFireBaseUniqueIdentifier).child('images');
+    var publicationImages = $firebaseObject(publicationImagesReference);
 
     /**
      * The ALL files, in queue to upload and those already in server.
@@ -179,6 +108,27 @@ angular.module('fileUpload',[])
      * @type {object}
      * */
     var filesCopy = angular.copy(files);
+
+    /**
+     * insert custom made file object in files object
+     * @param {String} reference
+     * @param {Object} fileObject
+     **/
+    var insertFile = function (reference,fileObject) {
+      files[reference] = fileObject;
+    };
+
+    /**
+     Receives the reference (UUID), and the new data object.
+     @param {String} reference is UUID string
+     @param {Object} newData
+     @return undefined
+     **/
+    var updateFileObj = function(reference,newData){
+      angular.forEach(newData, function (value, key) {
+        files[reference][key] = value;
+      });
+    };
 
     /**
      * Conserve aspect ratio of the original region. Useful when shrinking/enlarging
@@ -194,6 +144,30 @@ angular.module('fileUpload',[])
     var calculateAspectRatioFit = function (srcWidth, srcHeight, maxWidth, maxHeight) {
       var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
       return { width: srcWidth*ratio, height: srcHeight*ratio };
+    };
+
+    /**
+     Reduce imagen size and quality.
+     @param {String} imagen is a base64 string
+     @param {Number} width
+     @param {Number} height
+     @param {Number} quality from 0.1 to 1.0
+     @return Promise.<String>
+     **/
+    var generateThumbnail = function(imagen, width, height, quality){
+      var deferred          = $q.defer();
+      var canvasElement     = document.createElement('canvas');
+      var imagenElement     = document.createElement('img');
+      imagenElement.onload  = function(){
+        var  dimensions = calculateAspectRatioFit(imagenElement.width,imagenElement.height,width,height);
+        canvasElement.width   = dimensions.width;
+        canvasElement.height  = dimensions.height;
+        var context           = canvasElement.getContext('2d');
+        context.drawImage(imagenElement, 0, 0, dimensions.width, dimensions.height);
+        deferred.resolve(canvasElement.toDataURL('image/WebP', quality));
+      };
+      imagenElement.src = imagen;
+      return deferred.promise;
     };
 
     return {
@@ -241,14 +215,6 @@ angular.module('fileUpload',[])
         return $q.when(uuid);
       },
       /**
-       * insert custom made file object in files object
-       * @param {String} reference
-       * @param {Object} fileObject
-       **/
-      insertFile : function (reference,fileObject) {
-        files[reference] = fileObject;
-      },
-      /**
        Receives the reference (UUID) of the FILE object in files object. Create FileReader instance to read the file with that reference.
        @param  {String} reference is UUID string.
        @return  Promise.<String> . The Reading is a base64 string.
@@ -267,17 +233,7 @@ angular.module('fileUpload',[])
         reader.readAsDataURL(files[reference].file);
         return deferred.promise;
       },
-      /**
-       Receives the reference (UUID), and the new data object.
-       @param {String} reference is UUID string
-       @param {Object} newData
-       @return undefined
-       **/
-      updateFileObj : function(reference,newData){
-        angular.forEach(newData, function (value, key) {
-          files[reference][key] = value;
-        });
-      },
+      updateFileObj : updateFileObj,
       /**
        Remove ALL queue files to upload.
        @return Promise.<String>
@@ -291,7 +247,7 @@ angular.module('fileUpload',[])
         return $q.when('All queue files has been removed successfully.');
       },
       /**
-       TODO Remove the file with the reference provided in queue to upload or one that it is in the server.
+       Remove the file with the reference provided in queue to upload or one that it is in the server.
        @return Promise.<String>
        **/
       removeFile : function(reference){
@@ -299,59 +255,108 @@ angular.module('fileUpload',[])
         var fileName = files[reference].fileName;
         var message = 'The file: '+ fileName +', has been removed successfully.';
         if(files[reference].inServer){
-          $log.info('make http request');
-          deferred.resolve(message);
+          var imageObject = $firebaseObject(publicationImagesReference.child(reference));
+          imageObject.isDeleted = true;
+          imageObject.save().then(function(){
+            //delete files[reference];
+            deferred.resolve(message);
+          });
         }else{
           delete files[reference];
           deferred.resolve(message);
         }
         return deferred.promise;
       },
-      /**
-       Reduce imagen size and quality.
-       @param {String} imagen is a base64 string
-       @param {Number} width
-       @param {Number} height
-       @param {Number} quality from 0.1 to 1.0
-       @return Promise.<String>
-       **/
-      generateThumbnail : function(imagen, width, height, quality){
-        var deferred          = $q.defer();
-        var canvasElement     = document.createElement('canvas');
-        var imagenElement     = document.createElement('img');
-        imagenElement.onload  = function(){
-          var  dimensions = calculateAspectRatioFit(imagenElement.width,imagenElement.height,width,height);
-          canvasElement.width   = dimensions.width;
-          canvasElement.height  = dimensions.height;
-          var context           = canvasElement.getContext('2d');
-          context.drawImage(imagenElement, 0, 0, dimensions.width, dimensions.height);
-          deferred.resolve(canvasElement.toDataURL('image/jpeg', quality));
-        };
-        imagenElement.src = imagen;
+      setFireBaseUniqueIdentifier:function(uniqueIdentifier){
+
+      },
+      upload: function (fileObject,reference) {
+        var deferred = $q.defer();
+
+        $q.all({
+          reference: $q.when(reference),
+          fileName: $q.when(fileObject.fileName),
+          w200xh200Thumbnail: generateThumbnail(fileObject.preview,200,200,1.0),
+          w600xh600Thumbnail: generateThumbnail(fileObject.preview,600,600,1.0)
+        }).then(function(the){
+
+          var reference = the.reference;
+          var fileName  = the.fileName;
+
+          var imagesReference  = FireRef.child('images').child(reference);
+          var imageObj   = $firebaseObject(imagesReference);
+
+          imageObj.name       = fileName;
+          imageObj.thumbnails = {};
+
+          imageObj.thumbnails.w200xh200 = {};
+          imageObj.thumbnails.w200xh200.reference  = reference;
+          imageObj.thumbnails.w200xh200.base64     = the.w200xh200Thumbnail;
+
+          imageObj.thumbnails.w600xh600 = {};
+          imageObj.thumbnails.w600xh600.reference = reference;
+          imageObj.thumbnails.w600xh600.base64     = the.w600xh600Thumbnail;
+
+          publicationImages[reference]           = {};
+          publicationImages[reference].name      = fileName;
+          publicationImages[reference].isDeleted = false;
+
+          $q.all({
+            inImages: imageObj.$save().then(function(ref) {
+              var reference = ref.key();
+              updateFileObj(reference,{inServer:true});
+            }),
+            inPublications: publicationImages.$save()
+          }).then(function(){
+            deferred.resolve();
+          });
+        });
+
         return deferred.promise;
+      },
+      filesInServer: function(){
+        publicationImages.$watch(function() {
+          angular.forEach(publicationImages,function(fileObj,reference){
+            if(!fileObj.isDeleted){
+              if(!angular.isDefined(files[reference])){
+                var file = {
+                  fileName: fileObj.name,
+                  preview:  'images/loading.jpeg',
+                  inServer: true
+                };
+                insertFile(reference,file);
+                var w200xh200ThumbnailReference = FireRef.child('images').child(reference).child('thumbnails').child('w200xh200');
+                var imageObj  = $firebaseObject(w200xh200ThumbnailReference);
+                imageObj.$loaded(function(thumbnailObject){
+                  updateFileObj(thumbnailObject.reference,{preview:thumbnailObject.base64});
+                });
+              }
+            }
+          });
+        });
       }
     };
 
   }])
-  /**
-   * The fileUpload Directive Take the last files specified by the User.
-   * */
-  .directive('fileUpload',['$q','fileUploadService','$log',function($q,fileUploadService,$log){
+/**
+ * The fileUpload Directive Take the last files specified by the User.
+ * */
+  .directive('fileUpload',['$q','fileService','$log',function($q,fileService,$log){
     return {
       restrict: 'E',
       template: '<input type="file" multiple="multiple" accept="image/*" class="file-input">',
       replace:true,
       link: function (scope,element) {
-        fileUploadService.fileInputElement = element;
+        fileService.fileInputElement = element;
         element.on('change', function (event) {
           angular.forEach(event.target.files,function(file){
-            fileUploadService.newFile(file)
+            fileService.newFile(file)
               .then(function(reference){
                 // read file
-                return $q.all({reference: $q.when(reference), reading: fileUploadService.readFile(reference)});
+                return $q.all({reference: $q.when(reference), reading: fileService.readFile(reference)});
               }).then(function(the){
                 // update file object
-                fileUploadService.updateFileObj(the.reference,{preview: the.reading});
+                fileService.updateFileObj(the.reference,{preview: the.reading});
               },
               function(error){
                 $log.error('Error: ',error);
@@ -361,23 +366,23 @@ angular.module('fileUpload',[])
       }
     };
   }])
-  /**
-   * The fileUploadTrigger Directive Triggers click event on file Input Element.
-   * */
-  .directive('fileUploadTrigger',['fileUploadService',function(fileUploadService){
+/**
+ * The fileUploadTrigger Directive Triggers click event on file Input Element.
+ * */
+  .directive('fileUploadTrigger',['fileService',function(fileService){
     return {
       restrict: 'A',
       link: function (scope,element) {
         element.bind('click', function () {
-          fileUploadService.fileInputElement.click();
+          fileService.fileInputElement.click();
         });
       }
     };
   }])
-  /**
-   * The removeFiles return files object to original state.
-   * */
-  .directive('removeQueueFiles',['fileUploadService','notificationService',function(fileUploadService,notificationService){
+/**
+ * The removeFiles return files object to original state.
+ * */
+  .directive('removeQueueFiles',['fileService','notificationService',function(fileService,notificationService){
     return {
       restrict: 'A',
       scope:{
@@ -385,7 +390,7 @@ angular.module('fileUpload',[])
       },
       link: function (scope,element) {
         element.bind('click', function () {
-          fileUploadService.removeQueueFiles().then(function(message){
+          fileService.removeQueueFiles().then(function(message){
             scope.successMessage = scope.successMessage ? scope.successMessage : message;
             notificationService.success(scope.successMessage);
           });
@@ -393,10 +398,10 @@ angular.module('fileUpload',[])
       }
     };
   }])
-  /**
-   * The removeFile directive delete the specified file object in files object.
-   * */
-  .directive('removeFile',['fileUploadService','notificationService',function(fileUploadService,notificationService){
+/**
+ * The removeFile directive delete the specified file object in files object.
+ * */
+  .directive('removeFile',['fileService','notificationService',function(fileService,notificationService){
     return {
       restrict: 'A',
       scope:{
@@ -404,7 +409,7 @@ angular.module('fileUpload',[])
       },
       link: function (scope,element,attributes) {
         element.bind('click', function () {
-          fileUploadService.removeFile(attributes.removeFile).then(function(message){
+          fileService.removeFile(attributes.removeFile).then(function(message){
             scope.successMessage = scope.successMessage ? scope.successMessage : message;
             notificationService.success(scope.successMessage);
           });
