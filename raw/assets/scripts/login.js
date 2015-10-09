92bde3ca1c8695e7547e73e14e61c579bb2c9d0f
romelgomez <bmxquiksilver7185@gmail.com>
2015-10-08 23:54:09 -0430

    improving the login

--- raw/assets/scripts/login.js
+++ raw/assets/scripts/login.js
@@ -1,23 +1,24 @@
 'use strict';
 
 angular.module('login',['ngMessages','angular-loading-bar','validation.match','angular-loading-bar'])
-  .controller('LoginController', ['$scope','FireAuth','$location','$q','FireRef','notificationService','$log',function ($scope, FireAuth, $location, $q, FireRef, notificationService, $log) {
+  .controller('LoginController', ['$scope','FireAuth','$location','$q','FireRef','notificationService','$window','$log',function ($scope, FireAuth, $location, $q, FireRef, notificationService, $window, $log) {
     // Manages authentication to any active providers.
 
     var original = angular.copy($scope.model = {
       signIn:{
-        'email':'',
-        'password':''
+        email:'',
+        password:'',
+        rememberMe: false
       },
       register:{
-        'names':'',
-        'lastNames':'',
-        'email':'',
-        'password':'',
-        'samePassword':''
+        names:'',
+        lastNames:'',
+        email:'',
+        password:'',
+        samePassword:''
       },
       recoverAccount:{
-        'email':''
+        email:''
       }
     });
 
@@ -40,7 +41,7 @@ angular.module('login',['ngMessages','angular-loading-bar','validation.match','a
     };
 
     var redirect = function(){
-      $location.path('/#/new-publication');
+      $window.location = '#/new-publication'
     };
 
     /**
@@ -162,12 +163,12 @@ angular.module('login',['ngMessages','angular-loading-bar','validation.match','a
       }
     };
 
-
-    // TODO https://www.firebase.com/docs/web/guide/login/password.html  TO ADD 'remember' Optional Settings
     var signIn  = function(){
       var deferred  = $q.defer();
 
-      FireAuth.$authWithPassword({email: $scope.model.signIn.email, password: $scope.model.signIn.password})
+      var remember = $scope.model.signIn.rememberMe ? 'default' : 'sessionOnly';
+
+      FireAuth.$authWithPassword({email: $scope.model.signIn.email, password: $scope.model.signIn.password},{remember: remember})
           .then(function(authData){
 
             $log.info('authData', authData);
@@ -197,85 +198,42 @@ angular.module('login',['ngMessages','angular-loading-bar','validation.match','a
       }
     };
 
+    var recover = function (){
+      var deferred  = $q.defer();
+
+      FireAuth.$resetPassword({
+        email: $scope.model.recoverAccount.email
+      }).then(function() {
+        deferred.resolve();
+      }).catch(function(error) {
+        deferred.reject(error);
+      });
+
+      return deferred.promise;
+    };
+
     $scope.recover = function(){
       $scope.forms.recoverAccount.$setSubmitted(true);
       if($scope.forms.recoverAccount.$valid){
+
+        $scope.httpRequestPromise = recover()
+            .then(function(){
+              notificationService.success('Password reset email sent successfully!');
+            },function(error){
+              showError(error);
+            });
+
         $log.info('ok fromJson', angular.fromJson($scope.model.recoverAccount));
         $log.info('ok toJson', angular.toJson($scope.model.recoverAccount));
       }
     };
 
-    //$scope.oauthLogin = function(provider) {
-    //  $scope.err = null;
-    //  FireAuth.$authWithOAuthPopup(provider, {rememberMe: true}).then(redirect, showError);
-    //};
-    //
-    //$scope.anonymousLogin = function() {
-    //  $scope.err = null;
-    //  FireAuth.$authAnonymously({rememberMe: true}).then(redirect, showError);
-    //};
-    //
-    //$scope.passwordLogin = function(email, pass) {
-    //  $scope.err = null;
-    //  FireAuth.$authWithPassword({email: email, password: pass}, {rememberMe: true}).then(
-    //    redirect, showError
-    //  );
-    //};
-    //
-    //$scope.createAccount = function(email, pass, confirm) {
-    //  $scope.err = null;
-    //  if( !pass ) {
-    //    $scope.err = 'Please enter a password';
-    //  }
-    //  else if( pass !== confirm ) {
-    //    $scope.err = 'Passwords do not match';
-    //  }
-    //  else {
-    //    FireAuth.$createUser({email: email, password: pass})
-    //      .then(function () {
-    //        // authenticate so we have permission to write to Firebase
-    //        return FireAuth.$authWithPassword({email: email, password: pass}, {rememberMe: true});
-    //      })
-    //      .then(createProfile)
-    //      .then(redirect, showError);
-    //  }
-    //
-    //  function createProfile(user) {
-    //    var ref = FireRef.child('users', user.uid), def = $q.defer();
-    //    ref.set({email: email, name: firstPartOfEmail(email)}, function(err) {
-    //      $timeout(function() {
-    //        if( err ) {
-    //          def.reject(err);
-    //        }
-    //        else {
-    //          def.resolve(ref);
-    //        }
-    //      });
-    //    });
-    //    return def.promise;
-    //  }
-    //};
-    //
-    //function firstPartOfEmail(email) {
-    //  return ucfirst(email.substr(0, email.indexOf('@'))||'');
-    //}
-    //
-    //function ucfirst (str) {
-    //  // inspired by: http://kevin.vanzonneveld.net
-    //  str += '';
-    //  var f = str.charAt(0).toUpperCase();
-    //  return f + str.substr(1);
-    //}
-    //
-    //
-    //
-    //function redirect() {
-    //  $location.path('/account');
-    //}
-    //
-    //function showError(err) {
-    //  $scope.err = err;
-    //}
-
+    $scope.oauthLogin = function(provider) {
+      FireAuth.$authWithOAuthPopup(provider, {rememberMe: true})
+          .then(function(data){
+            $log.info('oauthLogin user data',data);
+          })
+          .then(redirect, showError);
+    };
 
   }]);
