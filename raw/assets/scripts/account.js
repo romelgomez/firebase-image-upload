396ddcd5700f27fae2622209e70d9f88dbc4b445
romelgomez <bmxquiksilver7185@gmail.com>
2015-10-10 22:50:25 -0430

    advances 2

--- raw/assets/scripts/account.js
+++ raw/assets/scripts/account.js
@@ -1,61 +1,127 @@
 'use strict';
 
 angular.module('account',[])
-  .controller('AccountController', function ($scope, user, FireAuth, FireRef, $firebaseObject, $timeout) {
-    // Provides rudimentary account management functions.
+  .controller('AccountController',['$scope','user', '$uibModal', function ($scope, user, $uibModal) {
 
     $scope.user = user;
 
-    $scope.logout = function() { FireAuth.$unauth(); };
-
-    $scope.messages = [];
-
-    var profile = $firebaseObject(FireRef.child('users/'+user.uid));
-
-    profile.$bindTo($scope, 'profile');
-
-
-    $scope.changePassword = function(oldPass, newPass, confirm) {
-      $scope.err = null;
-      if( !oldPass || !newPass ) {
-        error('Please enter all fields');
-      }
-      else if( newPass !== confirm ) {
-        error('Passwords do not match');
-      }
-      else {
-        FireAuth.$changePassword({email: profile.email, oldPassword: oldPass, newPassword: newPass})
-          .then(function() {
-            success('Password changed');
-          }, error);
-      }
+    $scope.changeProfileDetails = function(size){
+      $uibModal.open({
+        templateUrl: 'changeProfileDetailsModal.html',
+        controller: 'ChangeProfileDetailsController',
+        size: size
+      });
     };
 
-    $scope.changeEmail = function(pass, newEmail) {
-      $scope.err = null;
-      FireAuth.$changeEmail({password: pass, newEmail: newEmail, oldEmail: profile.email})
-        .then(function() {
-          profile.email = newEmail;
-          profile.$save();
-          success('Email changed');
-        })
-        .catch(error);
+    $scope.changePassword = function(size){
+      $uibModal.open({
+        templateUrl: 'changePasswordModal.html',
+        controller: 'ChangeAccountPasswordController',
+        size: size
+      });
     };
 
-    function error(err) {
-      alert(err, 'danger');
-    }
+    $scope.changeEmail = function(size){
+      $uibModal.open({
+        templateUrl: 'changeEmailModal.html',
+        controller: 'ChangeEmailAccountController',
+        size: size
+      });
+    };
 
-    function success(msg) {
-      alert(msg, 'success');
-    }
+    // Provides rudimentary account management functions.
+    //$scope.logout = function() { FireAuth.$unauth(); };
+    //
+    //$scope.messages = [];
+    //
+    //var profile = $firebaseObject(FireRef.child('users/'+user.uid));
+    //
+    //profile.$bindTo($scope, 'profile');
+    //
+    //
+    //$scope.changePassword = function(oldPass, newPass, confirm) {
+    //  $scope.err = null;
+    //  if( !oldPass || !newPass ) {
+    //    error('Please enter all fields');
+    //  }
+    //  else if( newPass !== confirm ) {
+    //    error('Passwords do not match');
+    //  }
+    //  else {
+    //    FireAuth.$changePassword({email: profile.email, oldPassword: oldPass, newPassword: newPass})
+    //      .then(function() {
+    //        success('Password changed');
+    //      }, error);
+    //  }
+    //};
+    //
+    //$scope.changeEmail = function(pass, newEmail) {
+    //  $scope.err = null;
+    //  FireAuth.$changeEmail({password: pass, newEmail: newEmail, oldEmail: profile.email})
+    //    .then(function() {
+    //      profile.email = newEmail;
+    //      profile.$save();
+    //      success('Email changed');
+    //    })
+    //    .catch(error);
+    //};
+    //
+    //function error(err) {
+    //  alert(err, 'danger');
+    //}
+    //
+    //function success(msg) {
+    //  alert(msg, 'success');
+    //}
+    //
+    //function alert(msg, type) {
+    //  var obj = {text: msg+'', type: type};
+    //  $scope.messages.unshift(obj);
+    //  $timeout(function() {
+    //    $scope.messages.splice($scope.messages.indexOf(obj), 1);
+    //  }, 10000);
+    //}
 
-    function alert(msg, type) {
-      var obj = {text: msg+'', type: type};
-      $scope.messages.unshift(obj);
-      $timeout(function() {
-        $scope.messages.splice($scope.messages.indexOf(obj), 1);
-      }, 10000);
-    }
+  }])
+  .controller('ChangeProfileDetailsController',['$scope','$modalInstance',function($scope,$modalInstance){
 
-  });
+    //var original = angular.copy($scope.model = {
+    //  signIn:{
+    //    email:'',
+    //    password:'',
+    //    rememberMe: false
+    //  },
+    //  register:{
+    //    names:'',
+    //    lastNames:'',
+    //    email:'',
+    //    password:'',
+    //    samePassword:''
+    //  },
+    //  recoverAccount:{
+    //    email:''
+    //  }
+    //});
+
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss();
+    };
+
+  }])
+  .controller('ChangeAccountPasswordController',['$scope',function($scope){
+
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss();
+    };
+
+  }])
+  .controller('ChangeEmailAccountController',['$scope',function($scope){
+
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss();
+    };
+
+  }]);
