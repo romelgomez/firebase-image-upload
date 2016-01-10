b7b33c71fa6c8c6767644f67a9def34240b640f2
romelgomez <bmxquiksilver7185@gmail.com>
2016-01-10 11:10:35 -0430

    split the code

--- public/assets/scripts/publications.js
+++ public/assets/scripts/publications.js
@@ -379,6 +379,8 @@ var publicationsModule = angular.module('publications',['categories','uuid','ngM
 
         loadPublication(publicationId)
           .then(function (the) {
+            $log.info('the.publication',the.publication);
+
             angular.forEach(the.publication, function ( value, key) {
               if(key !== 'releaseDate'){
                 $scope.publication.model[key] = value;
@@ -421,87 +423,4 @@ var publicationsModule = angular.module('publications',['categories','uuid','ngM
       $modalInstance.dismiss('This has be cancel');
     };
 
-  }])
-  .controller('ViewPublicationController',[
-    '$scope',
-    '$q',
-    '$window',
-    '$filter',
-    '$routeParams',
-    '$location',
-    '$http',
-    'FireRef',
-    '$firebaseArray',
-    '$firebaseObject',
-    'rfc4122',
-    'categoriesService',
-    'notificationService',
-    'Upload',
-    '$uibModal',
-    '$log',function($scope, $q, $window, $filter, $routeParams, $location, $http, FireRef, $firebaseArray, $firebaseObject, rfc4122, categoriesService, notificationService, $upload, $uibModal, $log){
-
-      var publicationsRef  = FireRef.child('publications');
-
-      $scope.publication = {
-        rawData: {},
-        images: []
-      };
-
-      function loadPublication(publicationId) {
-        var deferred   = $q.defer();
-
-        var publicationRef = publicationsRef.child(publicationId);
-        var publication    = $firebaseObject(publicationRef);
-
-        publication.$loaded(function(){
-          if (typeof publication.releaseDate === 'undefined'){
-            deferred.reject();
-          }  else {
-            deferred.resolve({publication:publication});
-          }
-        }, function (error) {
-          deferred.reject(error);
-        });
-
-        return deferred.promise;
-      }
-
-      if(angular.isDefined($routeParams.publicationId)){
-        $scope.httpRequestPromise = loadPublication($routeParams.publicationId)
-          .then(function(the){
-
-            $scope.publication.rawData = the.publication;
-
-            angular.forEach(the.publication.images, function(imageData,imageID){
-              imageData.$id = imageID;
-              if(imageID !== the.publication.featuredImageId){
-                $scope.publication.images.push(imageData);
-              }else{
-                $scope.publication.images.unshift(imageData)
-              }
-            });
-
-          }, function () {
-            notificationService.error('This action cannot be completed.');
-            $location.path('/');
-          });
-      }else{
-        notificationService.error('This action cannot be completed.');
-        $location.path('/');
-      }
-
-    }]);
-//.directive('barcode', function () {
-//  return {
-//    restrict: 'EA',
-//    scope: {
-//      data: '='
-//    },
-//    template: '<canvas id=barcode></canvas>',
-//    link: function(scope, element, attrs, ngModel){
-//      var canvas = element.find('canvas');
-//      //$log
-//
-//      JsBarcode(canvas[0], '74a2e884-053d-471e-b02f-df01a44142d8');
-//    }
-//}});
+  }]);
---
+++ public/assets/scripts/publications.view.js
@@ -0,0 +1,70 @@
+publicationsModule
+  .controller('ViewPublicationController',[
+    '$scope',
+    '$q',
+    '$window',
+    '$filter',
+    '$routeParams',
+    '$location',
+    '$http',
+    'FireRef',
+    '$firebaseArray',
+    '$firebaseObject',
+    'rfc4122',
+    'categoriesService',
+    'notificationService',
+    'Upload',
+    '$uibModal',
+    '$log',function($scope, $q, $window, $filter, $routeParams, $location, $http, FireRef, $firebaseArray, $firebaseObject, rfc4122, categoriesService, notificationService, $upload, $uibModal, $log){
+
+      var publicationsRef  = FireRef.child('publications');
+
+      $scope.publication = {
+        rawData: {},
+        images: []
+      };
+
+      function loadPublication(publicationId) {
+        var deferred   = $q.defer();
+
+        var publicationRef = publicationsRef.child(publicationId);
+        var publication    = $firebaseObject(publicationRef);
+
+        publication.$loaded(function(){
+          if (typeof publication.releaseDate === 'undefined'){
+            deferred.reject();
+          }  else {
+            deferred.resolve({publication:publication});
+          }
+        }, function (error) {
+          deferred.reject(error);
+        });
+
+        return deferred.promise;
+      }
+
+      if(angular.isDefined($routeParams.publicationId)){
+        $scope.httpRequestPromise = loadPublication($routeParams.publicationId)
+          .then(function(the){
+
+            $scope.publication.rawData = the.publication;
+
+            angular.forEach(the.publication.images, function(imageData,imageID){
+              imageData.$id = imageID;
+              if(imageID !== the.publication.featuredImageId){
+                $scope.publication.images.push(imageData);
+              }else{
+                $scope.publication.images.unshift(imageData)
+              }
+            });
+
+          }, function () {
+            notificationService.error('This action cannot be completed.');
+            $location.path('/');
+          });
+      }else{
+        notificationService.error('This action cannot be completed.');
+        $location.path('/');
+      }
+
+    }]);
