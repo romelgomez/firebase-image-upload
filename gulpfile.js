f2cbae54ad5595cf43285ee9b0a955304874e88f
romelgomez <bmxquiksilver7185@gmail.com>
2016-02-21 20:17:53 -0430

    advances with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,97 @@ var del     = require('del');
 var uglify  = require('gulp-uglify');
 var gulpif = require('gulp-if');
 var concat = require('gulp-concat');
+var notify = require("gulp-notify");
+var nodemon = require("gulp-nodemon");
+var htmlmin = require('gulp-htmlmin');
+var csso = require('gulp-csso');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano');
+var bowerFiles = require('main-bower-files');
+var inject = require('gulp-inject');
+var es = require('event-stream');
+var processhtml = require('gulp-processhtml');
+var useref = require('gulp-useref');
 
-function lint(files, options) {
-  return function() {
-    return gulp.src(files)
-      .pipe(eslint(options))
-      .pipe(eslint.format('stylish') );
-  };
-}
+var src = {
+  scripts : 'public/assets/scripts/**/*.js',
+  styles  : 'public/assets/styles/**/*.css',
+  images  : 'public/assets/images/**/*.*',
+  views   : 'public/assets/views/**/*.html'
+};
 
-gulp.task('lint', lint('public/assets/scripts/**/*.js'));
+var output = {
+  scripts : 'dist/assets/scripts',
+  styles  : 'dist/assets/styles',
+  images  : 'dist/assets/images',
+  views  : 'dist/assets/views',
+  dist    : 'dist'
+};
+
+gulp.task('lint', function() {
+  return gulp.src(src.scripts)
+    .pipe(eslint())
+    .pipe(eslint.format('stylish'));
+});
+
+gulp.task('views', function () {
+  return gulp.src(src.views)
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(output.views))
+});
+
+gulp.task('images', function () {
+  return gulp.src(src.images)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{removeViewBox: false}],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(output.images))
+});
+
+gulp.task('basic', function() {
+  return gulp.src('public/index.html')
+    .pipe(useref())
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.css', csso()))
+    .pipe(gulpif('*.html', htmlmin({collapseWhitespace: true})))
+    .pipe(gulp.dest(output.dist));
+});
+
+gulp.task('build', ['lint', 'basic', 'images', 'views'], function() {
+  return gulp.src('dist/**/*')
+    .pipe(size({title: 'build', gzip: true}));
+});
+
+gulp.task('start', function () {
+  nodemon({
+    script: 'server/app.js'
+    , ext: 'js html'
+    , env: { 'NODE_ENV': 'development' }
+  })
+});
+
+gulp.task('clean', del.bind(null, ['dist']));
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('build');
+});
+
+//gulp.task('files', function() {
+//  return gulp.src('public/**/*.*', { base: 'public' })
+//    .pipe(gulpif('*.js', uglify()))
+//    .pipe(gulp.dest('dist'));
+//});
+
+//gulp.task('files', function() {
+//  return gulp.src('public/**/*.*', { base: 'public' })
+//    .pipe(gulpif('*.js', uglify()))
+//    .pipe(concat('all.js'))
+//    .pipe(gulp.dest('dist'));
+//});
 
 //gulp.task('html', ['styles'], function() {
 //  return gulp.src('public/*.html')
@@ -27,38 +108,6 @@ gulp.task('lint', lint('public/assets/scripts/**/*.js'));
 //    .pipe($.useref())
 //    .pipe(gulp.dest('dist'));
 //});
-
-//gulp.task('files', function() {
-//  return gulp.src('public/**/*.*', { base: 'public' })
-//    .pipe(gulpif('*.js', uglify()))
-//    .pipe(gulp.dest('dist'));
-//});
-
-gulp.task('scripts', function() {
-  return gulp.src('public/**/*.js')
-    .pipe(uglify())
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('dist'));
-});
-
-//gulp.task('files', function() {
-//  return gulp.src('public/**/*.*', { base: 'public' })
-//    .pipe(gulpif('*.js', uglify()))
-//    .pipe(concat('all.js'))
-//    .pipe(gulp.dest('dist'));
-//});
-
-gulp.task('build', ['lint','scripts'], function() {
-  return gulp.src('dist/**/*')
-    .pipe(size({title: 'build', gzip: true}));
-});
-
-
-gulp.task('clean', del.bind(null, ['dist']));
-
-gulp.task('default', ['clean'], function() {
-  gulp.start('build');
-});
 
 
 /*
