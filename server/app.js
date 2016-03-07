333e500c18ef406cbb7d91814b45f4a67f47448c
romelgomez <bmxquiksilver7185@gmail.com>
2016-03-06 19:33:36 -0430

    test with getseojs

--- server/app.js
+++ server/app.js
@@ -6,8 +6,10 @@ var path = require('path'); // normalize the paths : http://stackoverflow.com/qu
 var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var port = process.env.PORT || 9090;
+var seojs = require('express-seojs');
 
 // configuration =================
+app.use(seojs('btp47oxcmqf7kkbi6z198f62f'));
 app.use(express.static(path.join(process.cwd(), 'dist'))); // set the static files location /public/img will be /img for users
 app.use('/bower_components',  express.static(path.join(process.cwd(), 'bower_components'))); // set the static files location of bower_components
 app.use(morgan('dev'));  // log every request to the console
@@ -15,7 +17,7 @@ app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
-app.use(require('prerender-node').set('prerenderToken', 'Ru0ksoIOrBjrq8Og6VGb'));
+//app.use(require('prerender-node').set('prerenderToken', 'Ru0ksoIOrBjrq8Og6VGb'));
 
 
 require('./routes')(app);
