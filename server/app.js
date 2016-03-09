1dae2d6f3ffb36b03917c037412d51bc17a97503
romelgomez <bmxquiksilver7185@gmail.com>
2016-03-09 18:13:34 -0430

    test with prerender.io

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({'extended':'true'})); // parse application/x-www-
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
-//app.use(require('prerender-node').set('prerenderToken', 'Ru0ksoIOrBjrq8Og6VGb'));
+app.use(require('prerender-node').set('prerenderToken', 'Ru0ksoIOrBjrq8Og6VGb'));
 
 
 require('./routes')(app);
